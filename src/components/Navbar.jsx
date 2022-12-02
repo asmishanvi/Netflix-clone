c5573bcf0a9d67168aec616954da6b8096be2de2
asmishanvi <asmishanvi18@gmail.com>
2022-12-02 23:49:30 +0530

    ui change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
         <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NEUFLEX</h1>
         <div>
-            <button className='text-white pr-4  hover:font-bold'>Sign In</button>
-            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white hover:bg-red-700  hover:font-bold'>Sign Up</button>
+            <button className='text-white pr-4 hover:font-bold'>Sign In</button>
+            <button className='bg-red-500 px-6 py-2 rounded cursor-pointer text-white hover:bg-red-700  hover:font-bold'>Sign Up</button>
         </div>
     </div>
   )
